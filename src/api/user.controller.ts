ef589777bc3c2329e4e882f511191a927c5d9cd7
yossi78 <yossi78@gmail.com>
2025-09-01 12:58:20 +0300

    add more controllers

--- src/api/user.controller.ts
+++ src/api/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Logger } from '@nestjs/common';
+import { Controller, Post, Put, Delete, Get, Param, Body, NotFoundException, Logger } from '@nestjs/common';
 import { UserService } from '../service/user.service';
 
 @Controller('users')
@@ -12,5 +12,39 @@ export class UserController {
   mcpCrud(@Body() body: any): any {
     this.logger.log('POST MCP operation');
     return this.service.mcpCrudOperation(body.operation, body.payload);
+  }
+
+  // Read all users
+  @Get()
+  getAll(): any {
+    this.logger.log('GET all users');
+    return this.service.getAll();
+  }
+
+  // Read user by id
+  @Get(':id')
+  getById(@Param('id') id: string): any {
+    this.logger.log(`GET user by id: ${id}`);
+    const user = this.service.getById(Number(id));
+    if (!user) throw new NotFoundException(`The user with id of ${id} does not exists`);
+    return user;
+  }
+
+  // Edit user
+  @Put(':id')
+  update(@Param('id') id: string, @Body() user: any): any {
+    this.logger.log(`PUT update user id: ${id}`);
+    const updated = this.service.update(Number(id), user);
+    if (!updated) throw new NotFoundException(`The user with id of ${id} does not exists`);
+    return updated;
+  }
+
+  // Remove user
+  @Delete(':id')
+  delete(@Param('id') id: string): any {
+    this.logger.log(`DELETE user id: ${id}`);
+    const success = this.service.delete(Number(id));
+    if (!success) throw new NotFoundException(`The user with id of ${id} does not exists`);
+    return { success };
   }
 }
